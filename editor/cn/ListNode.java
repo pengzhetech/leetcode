263d756e5d9085db3e03dcc34ac148d3dd523e17
pengzhe <pengzhepost@sina.com>
2020-06-29 09:02:38 +0800

    Initial commit

---
+++ editor/cn/ListNode.java
@@ -0,0 +1,22 @@
+/**
+ * @author pengzhe
+ * @date 2020/6/29 08:57
+ * @description
+ */
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
