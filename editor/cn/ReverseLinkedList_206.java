0d4b89fa69cd416a5df7f86915b43f37064528b1
pengzhe <pengzhepost@sina.com>
2020-07-03 21:46:13 +0800

    ReverseLinkedList_206

---
+++ editor/cn/ReverseLinkedList_206.java
@@ -0,0 +1,44 @@
+//反转一个单链表。 
+//
+// 示例: 
+//
+// 输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL 
+//
+// 进阶: 
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？ 
+// Related Topics 链表
+
+public class ReverseLinkedList_206 {
+    public static void main(String[] args) {
+        Solution solution = new ReverseLinkedList_206().new Solution();
+        int[] arr = {1, 2, 3, 4, 5};
+        ListNode listNode = new ListNode(arr);
+        System.out.println(solution.reverseList(listNode));
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode pre = null;
+            ListNode cur = head;
+            while (cur != null) {
+                ListNode tempNext = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = tempNext;
+            }
+            return pre;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
