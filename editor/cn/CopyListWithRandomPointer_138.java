143b7890ee985cb7e8b4cf36ed1e1cd35de824cb
pengzhe <pengzhepost@sina.com>
2020-07-05 11:15:17 +0800

    update structure

--- editor/cn/CopyListWithRandomPointer_138.java
+++ editor/cn/CopyListWithRandomPointer_138.java
@@ -83,14 +83,3 @@ public class CopyListWithRandomPointer_138 {
 
 }
 
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
---
+++ editor/cn/Node.java
@@ -0,0 +1,17 @@
+/**
+ * @author pengzhe
+ * @date 2020/7/5 11:13
+ * @description
+ */
+
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
