af67856454ef653d0b8ab6b2200ff45c9e84fb94
pengzhe <pengzhepost@sina.com>
2020-07-05 10:59:51 +0800

    SortList_148

---
+++ editor/cn/TreeNode.java
@@ -0,0 +1,15 @@
+/**
+ * @author pengzhe
+ * @date 2020/7/5 10:56
+ * @description
+ */
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
