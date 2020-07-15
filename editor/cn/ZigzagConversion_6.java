be2abad6fbcce2574bed5a50e30ef246b9e15603
pengzhe <pengzhepost@sina.com>
2020-07-15 21:10:11 +0800

    test plugin

---
+++ editor/cn/ZigzagConversion_6.java
@@ -0,0 +1,48 @@
+//将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。 
+//
+// 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下： 
+//
+// L   C   I   R
+//E T O E S I I G
+//E   D   H   N
+// 
+//
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。 
+//
+// 请你实现这个将字符串进行指定行数变换的函数： 
+//
+// string convert(string s, int numRows); 
+//
+// 示例 1: 
+//
+// 输入: s = "LEETCODEISHIRING", numRows = 3
+//输出: "LCIRETOESIIGEDHN"
+// 
+//
+// 示例 2: 
+//
+// 输入: s = "LEETCODEISHIRING", numRows = 4
+//输出: "LDREOEIIECIHNTSG"
+//解释:
+//
+//L     D     R
+//E   O E   I I
+//E C   I H   N
+//T     S     G 
+// Related Topics 字符串 
+// 👍 746 👎 0
+
+public class ZigzagConversion_6 {
+    public static void main(String[] args) {
+        Solution solution = new ZigzagConversion_6().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String convert(String s, int numRows) {
+            return null;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
