aba5faf41ba6542382507d78f46068bb824dcef0
pengzhe <pengzhepost@sina.com>
2020-07-19 18:50:12 +0800

    LongestPalindromicSubstring_5 use DP to solve this problem

--- editor/cn/LongestPalindromicSubstring_5.java
+++ editor/cn/LongestPalindromicSubstring_5.java
@@ -19,14 +19,59 @@ public class LongestPalindromicSubstring_5 {
     public static void main(String[] args) {
         Solution solution = new LongestPalindromicSubstring_5().new Solution();
 
+        String s = "abb2wrwerwerwerwererrrrrrc";
+
         LongestPalindromicSubstring_5 palindromicSubstring5 = new LongestPalindromicSubstring_5();
-        System.out.println(palindromicSubstring5.bruteForceLongestPalindrome("abb2wrwerwerwerwererrrrrrc"));
+        System.out.println("暴力法------->" + palindromicSubstring5.bruteForceLongestPalindrome(s));
+        System.out.println("动态规划------>" + solution.longestPalindrome(s));
+
+
     }
+
+    /**
+     * 动态规划解法
+     * 在头尾字符相等的情况下，里面子串的回文性质据定了整个子串的回文性质【状态转移方程】
+     *
+     * @return
+     */
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public String longestPalindrome(String s) {
-            return null;
+
+            if (s == null || s.length() == 0) {
+                return s;
+            }
+
+            int length = s.length();
+            char[] charArray = s.toCharArray();
+
+            //表示i,j是否是回文子串,闭区间i,j都能取到
+            boolean[][] isPalindrome = new boolean[length][length];
+
+            int maxLength = 1;
+            int beginIndex = 0;
+
+            //枚举所有子串
+            for (int j = 1; j < length; j++) {//表示子串的结束索引位置
+                for (int i = 0; i < j; i++) {
+                    if (charArray[i] != charArray[j]) {
+                        isPalindrome[i][j] = false;//首尾不等,肯定不是回文子串
+                    } else {
+                        if (j - i < 3) {//在收尾相等的情况下,长度小于3的子串肯定是回文的
+                            isPalindrome[i][j] = true;
+                        } else {
+                            //状态转移方程
+                            isPalindrome[i][j] = isPalindrome[i + 1][j - 1];
+                        }
+                    }
+                    if (isPalindrome[i][j] && j - i + 1 > maxLength) {
+                        maxLength = j - i + 1;
+                        beginIndex = i;
+                    }
+                }
+            }
+            return s.substring(beginIndex, beginIndex + maxLength);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
@@ -48,8 +93,8 @@ public class LongestPalindromicSubstring_5 {
         char[] charArray = s.toCharArray();
 
         //穷举s所有的子串
-        for (int i = 0; i < length - 1; i++) {//起始索引
-            for (int j = i + 1; j < length; j++) {//结束索引
+        for (int j = 1; j < length; j++) {//起始索引
+            for (int i = 0; i < j; i++) {//结束索引
                 if (isPalindrome(charArray, i, j) && j - i + 1 > maxLongestPalindromeLength) {
                     maxLongestPalindromeBeginIndex = i;
                     maxLongestPalindromeLength = j - i + 1;
