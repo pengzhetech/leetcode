f416c55c9123f457f72ca359bcf7c668ae9fcfdd
pengzhe <pengzhepost@sina.com>
2020-07-20 23:24:31 +0800

    Permutations_46

--- editor/cn/Permutations_46.java
+++ editor/cn/Permutations_46.java
@@ -15,19 +15,58 @@
 // Related Topics 回溯算法 
 // 👍 792 👎 0
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Permutations_46 {
     public static void main(String[] args) {
         Solution solution = new Permutations_46().new Solution();
+        System.out.println(solution.permute(new int[]{1, 2, 3}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
+
+    public class Solution {
+
         public List<List<Integer>> permute(int[] nums) {
-            return null;
+            // 首先是特判
+            int len = nums.length;
+            // 使用一个动态数组保存所有可能的全排列
+            List<List<Integer>> res = new ArrayList<>();
+
+            if (len == 0) {
+                return res;
+            }
+
+            boolean[] used = new boolean[len];
+            List<Integer> path = new ArrayList<>();
+
+            dfs(nums, len, 0, path, used, res);
+            return res;
         }
+
+        private void dfs(int[] nums, int len, int depth,
+                         List<Integer> path, boolean[] used,
+                         List<List<Integer>> res) {
+            if (depth == len) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+            for (int i = 0; i < len; i++) {
+                if (!used[i]) {
+                    path.add(nums[i]);
+                    used[i] = true;
+
+                    dfs(nums, len, depth + 1, path, used, res);
+                    // 注意：这里是状态重置，是从深层结点回到浅层结点的过程，代码在形式上和递归之前是对称的
+                    used[i] = false;
+                    path.remove(path.size() - 1);
+                }
+            }
+        }
+
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
