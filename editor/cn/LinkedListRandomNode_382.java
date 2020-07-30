a4e9e2c14e0702b1fdc7c439984d51eef978670c
pengzhe <pengzhepost@sina.com>
2020-07-31 00:01:44 +0800

    submit

---
+++ editor/cn/LinkedListRandomNode_382.java
@@ -0,0 +1,60 @@
+//给定一个单链表，随机选择链表的一个节点，并返回相应的节点值。保证每个节点被选的概率一样。 
+//
+// 进阶: 
+//如果链表十分大且长度未知，如何解决这个问题？你能否使用常数级空间复杂度实现？ 
+//
+// 示例: 
+//
+// 
+//// 初始化一个单链表 [1,2,3].
+//ListNode head = new ListNode(1);
+//head.next = new ListNode(2);
+//head.next.next = new ListNode(3);
+//Solution solution = new Solution(head);
+//
+//// getRandom()方法应随机返回1,2,3中的一个，保证每个元素被返回的概率相等。
+//solution.getRandom();
+// 
+// Related Topics 蓄水池抽样 
+// 👍 68 👎 0
+
+public class LinkedListRandomNode_382 {
+    public static void main(String[] args) {
+        // Solution solution = new LinkedListRandomNode_382().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+
+        /**
+         * @param head The linked list's head.
+         *             Note that the head is guaranteed to be not null, so it contains at least one node.
+         */
+        public Solution(ListNode head) {
+
+        }
+
+        /**
+         * Returns a random node's value.
+         */
+        public int getRandom() {
+            return 0;
+        }
+    }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ editor/cn/RangeSumQueryMutable_307.java
@@ -0,0 +1,52 @@
+//给定一个整数数组 nums，求出数组从索引 i 到 j (i ≤ j) 范围内元素的总和，包含 i, j 两点。 
+//
+// update(i, val) 函数可以通过将下标为 i 的数值更新为 val，从而对数列进行修改。 
+//
+// 示例: 
+//
+// Given nums = [1, 3, 5]
+//
+//sumRange(0, 2) -> 9
+//update(1, 2)
+//sumRange(0, 2) -> 8
+// 
+//
+// 说明: 
+//
+// 
+// 数组仅可以在 update 函数下进行修改。 
+// 你可以假设 update 函数与 sumRange 函数的调用次数是均匀分布的。 
+// 
+// Related Topics 树状数组 线段树 
+// 👍 152 👎 0
+
+public class RangeSumQueryMutable_307 {
+    public static void main(String[] args) {
+      //  Solution solution = new RangeSumQueryMutable_307().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class NumArray {
+
+        public NumArray(int[] nums) {
+
+        }
+
+        public void update(int i, int val) {
+
+        }
+
+        public int sumRange(int i, int j) {
+            return 0;
+        }
+    }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
