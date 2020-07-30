b8168025867386ae84b9d9707c4af56331305c66
pengzhe <pengzhepost@sina.com>
2020-07-30 23:48:37 +0800

    ReverseWordsInAStringIii_557

---
+++ editor/cn/ReverseWordsInAStringIii_557.java
@@ -0,0 +1,40 @@
+//给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。 
+//
+// 示例 1: 
+//
+// 
+//输入: "Let's take LeetCode contest"
+//输出: "s'teL ekat edoCteeL tsetnoc" 
+// 
+//
+// 注意：在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。 
+// Related Topics 字符串 
+// 👍 206 👎 0
+
+public class ReverseWordsInAStringIii_557 {
+    public static void main(String[] args) {
+        Solution solution = new ReverseWordsInAStringIii_557().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    public class Solution {
+        public String reverseWords(String input) {
+            final StringBuilder result = new StringBuilder();
+            final StringBuilder word = new StringBuilder();
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) != ' ') {
+                    word.append(input.charAt(i));
+                } else {
+                    result.append(word.reverse());
+                    result.append(" ");
+                    word.setLength(0);
+                }
+            }
+            result.append(word.reverse());
+            return result.toString();
+        }
+    }
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
