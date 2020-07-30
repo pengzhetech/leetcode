b47bbef3683949ab0e0750a3467928b25c5ca416
pengzhe <pengzhepost@sina.com>
2020-07-30 23:52:26 +0800

    MoveZeroes_283

--- editor/cn/MoveZeroes_283.java
+++ editor/cn/MoveZeroes_283.java
@@ -19,11 +19,23 @@ public class MoveZeroes_283{
            Solution solution = new MoveZeroes_283().new Solution();
       }
       //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public void moveZeroes(int[] nums) {
-
-    }
-}
+      class Solution {
+          public void moveZeroes(int[] nums) {
+              if(nums==null) {
+                  return;
+              }
+              //两个指针i和j
+              int j = 0;
+              for(int i=0;i<nums.length;i++) {
+                  //当前元素!=0，就把其交换到左边，等于0的交换到右边
+                  if(nums[i]!=0) {
+                      int tmp = nums[i];
+                      nums[i] = nums[j];
+                      nums[j++] = tmp;
+                  }
+              }
+          }
+      }
 //leetcode submit region end(Prohibit modification and deletion)
 
   }
