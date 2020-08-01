aba4c1717e936ebfadddc0c1da8d8d51007e6c5b
pengzhe <pengzhepost@sina.com>
2020-08-01 20:34:15 +0800

    MergeTwoSortedLists_21

--- editor/cn/MergeTwoSortedLists_21.java
+++ editor/cn/MergeTwoSortedLists_21.java
@@ -33,22 +33,21 @@ public class MergeTwoSortedLists_21 {
      */
     class Solution {
         public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
             ListNode dummy = new ListNode(-1);
 
             ListNode cur = dummy;
+
             while (l1 != null && l2 != null) {
                 if (l1.val <= l2.val) {
-                    cur.next = l1;
+                    cur.next = new ListNode(l1.val);
                     l1 = l1.next;
                 } else {
-                    cur.next = l2;
+                    cur.next = new ListNode(l2.val);
                     l2 = l2.next;
                 }
                 cur = cur.next;
             }
-
-            // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
-            cur.next = l1 == null ? l2 : l1;
 
             return dummy.next;
         }
@@ -56,4 +55,24 @@ public class MergeTwoSortedLists_21 {
 
 //leetcode submit region end(Prohibit modification and deletion)
 
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        cur.next = l1 == null ? l2 : l1;
+
+        return dummy.next;
+    }
 }
