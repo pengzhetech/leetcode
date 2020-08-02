b152f24505f96d58317d5d9643bbca4d7454335f
pengzhe <pengzhepost@sina.com>
2020-08-02 18:00:53 +0800

    submit subject

--- editor/cn/ListNodeTest.java
+++ editor/cn/ListNodeTest.java
@@ -12,15 +12,35 @@ public class ListNodeTest {
 
         int[] arr = {1, 2, 3, 4, 5, 6};
         ListNode listNode = new ListNode(arr);
-        System.out.println(listNode);
 
+        System.out.println(testList(listNode));
 
-        System.out.println(endOfFirstHalf(listNode));
+        // System.out.println(endOfFirstHalf(listNode));
 
        /* ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
         test(head);*/
+    }
+
+
+    static ListNode testList(ListNode head) {
+
+        System.out.println("输入的链表::" + head);
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = new ListNode(100);
+
+
+        System.out.println("新链表::" + dummy.next);
+
+        return dummy.next;
     }
 
 
@@ -54,4 +74,5 @@ public class ListNodeTest {
         System.out.println("cur::" + cur);
         System.out.println("原链表::" + head);
     }
+
 }
