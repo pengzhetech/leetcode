5656597f03d9a73da52d988e0fbc22995d980895
pengzhe <pengzhepost@sina.com>
2020-08-05 23:59:12 +0800

    SymmetricTree_101

--- editor/cn/SymmetricTree_101.java
+++ editor/cn/SymmetricTree_101.java
@@ -47,8 +47,16 @@ public class SymmetricTree_101 {
      */
     class Solution {
         public boolean isSymmetric(TreeNode root) {
-            return false;
+            return isMirror(root, root);
 
+        }
+
+        private boolean isMirror(TreeNode tree1, TreeNode tree2) {
+            if (tree1 == null && tree2 == null) return true;
+            if (tree1 == null || tree2 == null) return false;
+            return tree1.val == tree2.val
+                    && isMirror(tree1.left, tree2.right)
+                    && isMirror(tree1.right, tree2.left);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
