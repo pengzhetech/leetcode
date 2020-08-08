dd19f5ffe65825d8a7568973105d9e16eedf51fd
pengzhe <pengzhepost@sina.com>
2020-08-08 15:38:05 +0800

    LowestCommonAncestorOfABinaryTree_236

--- editor/cn/LowestCommonAncestorOfABinaryTree_236.java
+++ editor/cn/LowestCommonAncestorOfABinaryTree_236.java
@@ -51,9 +51,15 @@ public class LowestCommonAncestorOfABinaryTree_236 {
      */
     class Solution {
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-            return null;
+            if (root == null || root == p || root == q) return root;
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (left == null) return right;
+            if (right == null) return left;
+            return root;
         }
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
