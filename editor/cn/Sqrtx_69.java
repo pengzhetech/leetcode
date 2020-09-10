5ce6e530d1ac9fa5635c119df555dcca31ee16f5
pengzhe <pengzhepost@sina.com>
2020-09-11 00:17:18 +0800

    Sqrtx_69
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/Sqrtx_69.java
+++ editor/cn/Sqrtx_69.java
@@ -28,7 +28,21 @@ public class Sqrtx_69 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int mySqrt(int x) {
-            return 0;
+
+            int left = 0;
+            int right = x;
+            int ans = -1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if ((long) mid * mid <= x) {
+                    ans = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return ans;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
