fd38047f4a5edfcd9ac70bf3cd0f8a096d123162
pengzhe <pengzhepost@sina.com>
2020-09-11 23:29:48 +0800

    FindPeakElement_162
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/FindPeakElement_162.java
+++ editor/cn/FindPeakElement_162.java
@@ -34,7 +34,18 @@ public class FindPeakElement_162 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int findPeakElement(int[] nums) {
-            return 0;
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                //处于上升趋势
+                if (nums[mid] < nums[mid + 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
