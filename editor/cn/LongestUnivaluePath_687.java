85c179291c58d546d88f6ab1f02813aacb83254f
pengzhe <pengzhepost@sina.com>
2020-09-12 17:13:26 +0800

    recursion
    Signed-off-by: pengzhe <pengzhepost@sina.com>

---
+++ editor/cn/LongestUnivaluePath_687.java
@@ -0,0 +1,67 @@
+//给定一个二叉树，找到最长的路径，这个路径中的每个节点具有相同值。 这条路径可以经过也可以不经过根节点。 
+//
+// 注意：两个节点之间的路径长度由它们之间的边数表示。 
+//
+// 示例 1: 
+//
+// 输入: 
+//
+// 
+//              5
+//             / \
+//            4   5
+//           / \   \
+//          1   1   5
+// 
+//
+// 输出: 
+//
+// 
+//2
+// 
+//
+// 示例 2: 
+//
+// 输入: 
+//
+// 
+//              1
+//             / \
+//            4   5
+//           / \   \
+//          4   4   5
+// 
+//
+// 输出: 
+//
+// 
+//2
+// 
+//
+// 注意: 给定的二叉树不超过10000个结点。 树的高度不超过1000。 
+// Related Topics 树 递归 
+// 👍 351 👎 0
+
+public class LongestUnivaluePath_687 {
+    public static void main(String[] args) {
+        Solution solution = new LongestUnivaluePath_687().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public int longestUnivaluePath(TreeNode root) {
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
