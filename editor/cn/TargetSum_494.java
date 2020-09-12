a6acbe9b53585776e575e94c8ffa65edbed6dac4
pengzhe <pengzhepost@sina.com>
2020-09-12 17:08:50 +0800

    TargetSum_494
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/TargetSum_494.java
+++ editor/cn/TargetSum_494.java
@@ -38,9 +38,20 @@ public class TargetSum_494 {
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
+    public class Solution {
         public int findTargetSumWays(int[] nums, int S) {
-            return 0;
+            int[][] dp = new int[nums.length][2001];
+            dp[0][nums[0] + 1000] = 1;
+            dp[0][-nums[0] + 1000] += 1;
+            for (int i = 1; i < nums.length; i++) {
+                for (int sum = -1000; sum <= 1000; sum++) {
+                    if (dp[i - 1][sum + 1000] > 0) {
+                        dp[i][sum + nums[i] + 1000] += dp[i - 1][sum + 1000];
+                        dp[i][sum - nums[i] + 1000] += dp[i - 1][sum + 1000];
+                    }
+                }
+            }
+            return S > 1000 ? 0 : dp[nums.length - 1][S + 1000];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
