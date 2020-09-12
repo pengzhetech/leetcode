fbc73721687a5ebe34942545f020cff87924016d
pengzhe <pengzhepost@sina.com>
2020-09-12 15:30:28 +0800

    FlattenAMultilevelDoublyLinkedList_430
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/FlattenAMultilevelDoublyLinkedList_430.java
+++ editor/cn/FlattenAMultilevelDoublyLinkedList_430.java
@@ -94,11 +94,69 @@ class Node {
 };
 */
 
+    /*
+ // Definition for a Node.
+ class Node {
+     public int val;
+     public Node prev;
+     public Node next;
+     public Node child;
+
+     public Node() {}
+
+     public Node(int _val,Node _prev,Node _next,Node _child) {
+         val = _val;
+         prev = _prev;
+         next = _next;
+         child = _child;
+     }
+ };
+ */
     class Solution {
-        public RandomNode flatten(RandomNode head) {
-            return null;
+        public Node flatten(Node head) {
+            if (head == null) return head;
+            // pseudo head to ensure the `prev` pointer is never none
+            Node pseudoHead = new Node(0, null, head, null);
+
+            flattenDFS(pseudoHead, head);
+
+            // detach the pseudo head from the real head
+            pseudoHead.next.prev = null;
+            return pseudoHead.next;
+        }
+
+        /* return the tail of the flatten list */
+        public Node flattenDFS(Node prev, Node curr) {
+            if (curr == null) return prev;
+            curr.prev = prev;
+            prev.next = curr;
+
+            // the curr.next would be tempered in the recursive function
+            Node tempNext = curr.next;
+
+            Node tail = flattenDFS(curr, curr.child);
+            curr.child = null;
+
+            return flattenDFS(tail, tempNext);
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+
+        public Node() {
+        }
+
+        public Node(int _val, Node _prev, Node _next, Node _child) {
+            val = _val;
+            prev = _prev;
+            next = _next;
+            child = _child;
+        }
+    }
 
 }
