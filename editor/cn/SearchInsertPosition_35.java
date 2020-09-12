0c1cc337fb20a780ce9877303b2c7c0524b01496
pengzhe <pengzhepost@sina.com>
2020-09-12 15:14:19 +0800

    SearchInsertPosition_35
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/SearchInsertPosition_35.java
+++ editor/cn/SearchInsertPosition_35.java
@@ -34,9 +34,32 @@ public class SearchInsertPosition_35 {
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
+    public class Solution {
+
         public int searchInsert(int[] nums, int target) {
-            return 0;
+            int len = nums.length;
+            if (len == 0) {
+                return 0;
+            }
+
+            // 特判
+            if (nums[len - 1] < target) {
+                return len;
+            }
+            int left = 0;
+            int right = len - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                // 严格小于 target 的元素一定不是解
+                if (nums[mid] < target) {
+                    // 下一轮搜索区间是 [mid + 1, right]
+                    left = mid + 1;
+                } else {
+                    // 下一轮搜索区间是 [left, mid]
+                    right = mid;
+                }
+            }
+            return left;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
