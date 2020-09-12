0421abc511821b581e957b2059268a3187473ba1
pengzhe <pengzhepost@sina.com>
2020-09-12 10:33:09 +0800

    PowxN_50
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/PowxN_50.java
+++ editor/cn/PowxN_50.java
@@ -34,8 +34,17 @@ public class PowxN_50 {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        public double quickMul(double x, long N) {
+            if (N == 0) {
+                return 1.0;
+            }
+            double y = quickMul(x, N / 2);
+            return N % 2 == 0 ? y * y : y * y * x;
+        }
+
         public double myPow(double x, int n) {
-            return 0;
+            long N = n;
+            return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
