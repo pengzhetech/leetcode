a0d197051f87ef3d41faff63664a83a76121b4d0
pengzhe <pengzhepost@sina.com>
2020-09-14 22:08:06 +0800

    KThSymbolInGrammar_779
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/KThSymbolInGrammar_779.java
+++ editor/cn/KThSymbolInGrammar_779.java
@@ -42,9 +42,17 @@ public class KThSymbolInGrammar_779 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int kthGrammar(int N, int K) {
-            return 0;
+            int[] lastrow = new int[1 << N];
+            for (int i = 1; i < N; ++i) {
+                for (int j = (1 << (i - 1)) - 1; j >= 0; --j) {
+                    lastrow[2 * j] = lastrow[j];
+                    lastrow[2 * j + 1] = 1 - lastrow[j];
+                }
+            }
+            return lastrow[K - 1];
         }
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
