7089222c65190c1cf14f65f08514ef6e90d9e101
pengzhe <pengzhepost@sina.com>
2020-09-14 22:12:52 +0800

    subject
    Signed-off-by: pengzhe <pengzhepost@sina.com>

---
+++ editor/cn/KeyboardRow_500.java
@@ -0,0 +1,37 @@
+//给定一个单词列表，只返回可以使用在键盘同一行的字母打印出来的单词。键盘如下图所示。 
+//
+// 
+//
+// 
+//
+// 
+//
+// 示例： 
+//
+// 输入: ["Hello", "Alaska", "Dad", "Peace"]
+//输出: ["Alaska", "Dad"]
+// 
+//
+// 
+//
+// 注意： 
+//
+// 
+// 你可以重复使用键盘上同一字符。 
+// 你可以假设输入的字符串将只包含字母。 
+// Related Topics 哈希表 
+// 👍 105 👎 0
+
+public class KeyboardRow_500{
+      public static void main(String[] args) {
+           Solution solution = new KeyboardRow_500().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String[] findWords(String[] words) {
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
