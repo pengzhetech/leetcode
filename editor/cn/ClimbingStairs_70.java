0d84eefc04dac931f46ef32698106051015e93e1
pengzhe <pengzhepost@sina.com>
2020-09-14 22:05:37 +0800

    ClimbingStairs_70
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/ClimbingStairs_70.java
+++ editor/cn/ClimbingStairs_70.java
@@ -32,7 +32,13 @@ public class ClimbingStairs_70 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int climbStairs(int n) {
-            return 0;
+            int p = 0, q = 0, r = 1;
+            for (int i = 1; i <= n; ++i) {
+                p = q;
+                q = r;
+                r = p + q;
+            }
+            return r;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
