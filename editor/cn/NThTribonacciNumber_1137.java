89c305c94a824281410811034918c94b5850507c
pengzhe <pengzhepost@sina.com>
2020-09-17 23:17:20 +0800

    NThTribonacciNumber_1137
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/NThTribonacciNumber_1137.java
+++ editor/cn/NThTribonacciNumber_1137.java
@@ -38,11 +38,33 @@ public class NThTribonacciNumber_1137 {
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
-    class Solution {
-        public int tribonacci(int n) {
-            return 0;
+     class Tri {
+        private int n = 38;
+        public int[] nums = new int[n];
+
+        int helper(int k) {
+            if (k == 0) return 0;
+            if (nums[k] != 0) return nums[k];
+
+            nums[k] = helper(k - 1) + helper(k - 2) + helper(k - 3);
+            return nums[k];
+        }
+
+        Tri() {
+            nums[1] = 1;
+            nums[2] = 1;
+            helper(n - 1);
         }
     }
+
+    class Solution {
+        public Tri t = new Tri();
+
+        public int tribonacci(int n) {
+            return t.nums[n];
+        }
+    }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
