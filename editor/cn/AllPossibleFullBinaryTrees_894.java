c5d13e9ec746e72d972984526dbffc60b814088d
pengzhe <pengzhepost@sina.com>
2020-09-17 23:13:25 +0800

    AllPossibleFullBinaryTrees_894
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/AllPossibleFullBinaryTrees_894.java
+++ editor/cn/AllPossibleFullBinaryTrees_894.java
@@ -27,6 +27,7 @@
 // Related Topics 树 递归 
 // 👍 140 👎 0
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AllPossibleFullBinaryTrees_894 {
@@ -44,9 +45,39 @@ public class AllPossibleFullBinaryTrees_894 {
      * TreeNode(int x) { val = x; }
      * }
      */
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
     class Solution {
         public List<TreeNode> allPossibleFBT(int N) {
-            return null;
+            if (N % 2 == 0) {
+                return new ArrayList<>(0);
+            }
+            List<TreeNode> list = new ArrayList<>();
+            if (N == 1) {
+                list.add(new TreeNode(0));
+                return list;
+            }
+            N--;
+            for (int i = 1; i < N; i += 2) {
+                List<TreeNode> left = allPossibleFBT(i);
+                List<TreeNode> right = allPossibleFBT(N - i);
+                for (TreeNode l : left) {
+                    for (TreeNode r : right) {
+                        TreeNode node = new TreeNode(0);
+                        node.left = l;
+                        node.right = r;
+                        list.add(node);
+                    }
+                }
+            }
+            return list;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
