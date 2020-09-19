7630fe1bc339b2583dc4c14fb6127aa67d5127df
pengzhe <pengzhepost@sina.com>
2020-09-19 23:23:29 +0800

    InsertIntoABinarySearchTree_701
    Signed-off-by: pengzhe <pengzhepost@sina.com>

---
+++ editor/cn/InsertIntoABinarySearchTree_701.java
@@ -0,0 +1,77 @@
+//给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 保证原始二叉搜索树中不存在新值。 
+//
+// 注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回任意有效的结果。 
+//
+// 例如, 
+//
+// 
+//给定二叉搜索树:
+//
+//        4
+//       / \
+//      2   7
+//     / \
+//    1   3
+//
+//和 插入的值: 5
+// 
+//
+// 你可以返回这个二叉搜索树: 
+//
+// 
+//         4
+//       /   \
+//      2     7
+//     / \   /
+//    1   3 5
+// 
+//
+// 或者这个树也是有效的: 
+//
+// 
+//         5
+//       /   \
+//      2     7
+//     / \   
+//    1   3
+//         \
+//          4
+// 
+// Related Topics 树 
+// 👍 84 👎 0
+
+public class InsertIntoABinarySearchTree_701 {
+    public static void main(String[] args) {
+        Solution solution = new InsertIntoABinarySearchTree_701().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public TreeNode insertIntoBST(TreeNode root, int val) {
+            if (root == null) return new TreeNode(val);
+
+            // insert into the right subtree
+            if (val > root.val) root.right = insertIntoBST(root.right, val);
+                // insert into the left subtree
+            else root.left = insertIntoBST(root.left, val);
+            return root;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
