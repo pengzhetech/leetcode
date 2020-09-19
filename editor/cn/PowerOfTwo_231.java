546c2ea9e1363593ca01c2710866520d3d0b4698
pengzhe <pengzhepost@sina.com>
2020-09-19 23:24:19 +0800

    PowerOfTwo_231
    Signed-off-by: pengzhe <pengzhepost@sina.com>

---
+++ editor/cn/PowerOfTwo_231.java
@@ -0,0 +1,35 @@
+//给定一个整数，编写一个函数来判断它是否是 2 的幂次方。 
+//
+// 示例 1: 
+//
+// 输入: 1
+//输出: true
+//解释: 20 = 1 
+//
+// 示例 2: 
+//
+// 输入: 16
+//输出: true
+//解释: 24 = 16 
+//
+// 示例 3: 
+//
+// 输入: 218
+//输出: false 
+// Related Topics 位运算 数学 
+// 👍 242 👎 0
+
+public class PowerOfTwo_231 {
+    public static void main(String[] args) {
+        Solution solution = new PowerOfTwo_231().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isPowerOfTwo(int n) {
+            return false;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
