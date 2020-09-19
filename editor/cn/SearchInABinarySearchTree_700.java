ae7a2f05df62a3ef1edcf4d117c342109b6d0f95
pengzhe <pengzhepost@sina.com>
2020-09-19 23:08:34 +0800

    SearchInABinarySearchTree_700
    Signed-off-by: pengzhe <pengzhepost@sina.com>

---
+++ editor/cn/SearchInABinarySearchTree_700.java
@@ -0,0 +1,52 @@
+//给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。 
+//
+// 例如， 
+//
+// 
+//给定二叉搜索树:
+//
+//        4
+//       / \
+//      2   7
+//     / \
+//    1   3
+//
+//和值: 2
+// 
+//
+// 你应该返回如下子树: 
+//
+// 
+//      2     
+//     / \   
+//    1   3
+// 
+//
+// 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。 
+// Related Topics 树 
+// 👍 89 👎 0
+
+public class SearchInABinarySearchTree_700 {
+    public static void main(String[] args) {
+        Solution solution = new SearchInABinarySearchTree_700().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            if (root == null || root.val == val) return root;
+            return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
