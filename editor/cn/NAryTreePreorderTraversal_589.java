8a0ab39ce67186aa93f1b172ec054a3de92589b6
pengzhe <pengzhepost@sina.com>
2020-09-20 20:09:05 +0800

    NAryTreePreorderTraversal_589
    Signed-off-by: pengzhe <pengzhepost@sina.com>

---
+++ editor/cn/NAryTreePreorderTraversal_589.java
@@ -0,0 +1,89 @@
+//给定一个 N 叉树，返回其节点值的前序遍历。 
+//
+// 例如，给定一个 3叉树 : 
+//
+// 
+//
+// 
+//
+// 
+//
+// 返回其前序遍历: [1,3,5,6,2,4]。 
+//
+// 
+//
+// 说明: 递归法很简单，你可以使用迭代法完成此题吗? Related Topics 树 
+// 👍 102 👎 0
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NAryTreePreorderTraversal_589 {
+    public static void main(String[] args) {
+        Solution solution = new NAryTreePreorderTraversal_589().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+    class Solution {
+        public List<Integer> preorder(Node root) {
+            LinkedList<Node> stack = new LinkedList<>();
+            LinkedList<Integer> output = new LinkedList<>();
+            if (root == null) {
+                return output;
+            }
+
+            stack.add(root);
+            while (!stack.isEmpty()) {
+                Node node = stack.pollLast();
+                output.add(node.val);
+                Collections.reverse(node.children);
+                for (Node item : node.children) {
+                    stack.add(item);
+                }
+            }
+            return output;
+        }
+
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    ;
+
+}
+
