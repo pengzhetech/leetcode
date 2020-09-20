21cbd7f81273ff5001076dc307e3c0fa514877c4
pengzhe <pengzhepost@sina.com>
2020-09-20 20:15:28 +0800

    NAryTreePreorderTraversal_589
    Signed-off-by: pengzhe <pengzhepost@sina.com>

---
+++ editor/cn/NAryTreeLevelOrderTraversal_429.java
@@ -0,0 +1,99 @@
+//给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。 
+//
+// 例如，给定一个 3叉树 : 
+//
+// 
+//
+// 
+//
+// 
+//
+// 返回其层序遍历: 
+//
+// [
+//     [1],
+//     [3,2,4],
+//     [5,6]
+//]
+// 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 树的深度不会超过 1000。 
+// 树的节点总数不会超过 5000。 
+// Related Topics 树 广度优先搜索 
+// 👍 109 👎 0
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class NAryTreeLevelOrderTraversal_429 {
+    public static void main(String[] args) {
+        Solution solution = new NAryTreeLevelOrderTraversal_429().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+// This code is a modified version of the code posted by
+// #zzzliu on the discussion forums.
+    class Solution {
+
+        public List<List<Integer>> levelOrder(Node root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) return result;
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                List<Integer> level = new ArrayList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    Node node = queue.poll();
+                    level.add(node.val);
+                    queue.addAll(node.children);
+                }
+                result.add(level);
+            }
+            return result;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+}
