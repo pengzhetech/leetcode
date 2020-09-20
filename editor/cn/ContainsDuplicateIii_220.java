0da3160203681d9eddd669a3fe6f6f903b60437e
pengzhe <pengzhepost@sina.com>
2020-09-20 19:35:19 +0800

    ContainsDuplicateIii_220
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/ContainsDuplicateIii_220.java
+++ editor/cn/ContainsDuplicateIii_220.java
@@ -31,25 +31,27 @@ public class ContainsDuplicateIii_220 {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
         public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-            TreeSet<Integer> set = new TreeSet<>();
-            for (int i = 0; i < nums.length; ++i) {
-                // Find the successor of current element
-                Integer s = set.ceiling(nums[i]);
-                if (s != null && s <= nums[i] + t) return true;
-
-                // Find the predecessor of current element
-                Integer g = set.floor(nums[i]);
-                if (g != null && nums[i] <= g + t) return true;
-
-                set.add(nums[i]);
-                if (set.size() > k) {
-                    set.remove(nums[i - k]);
+            // 滑动窗口结合查找表，此时滑动窗口即为查找表本身（控制查找表的大小即可控制窗口大小）
+            TreeSet<Long> set = new TreeSet<>();
+            for (int i = 0; i < nums.length; i++) {
+                // 边添加边查找
+                // 查找表中是否有大于等于 nums[i] - t 且小于等于 nums[i] + t 的值
+                Long ceiling = set.ceiling((long) nums[i] - (long) t);
+                if (ceiling != null && ceiling <= ((long) nums[i] + (long) t)) {
+                    return true;
+                }
+                // 添加后，控制查找表（窗口）大小，移除窗口最左边元素
+                set.add((long) nums[i]);
+                if (set.size() == k + 1) {
+                    set.remove((long) nums[i - k]);
                 }
             }
             return false;
         }
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
