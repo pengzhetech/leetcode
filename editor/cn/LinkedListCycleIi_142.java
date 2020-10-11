27082880bfa80deb6a7025e995927da1ba6434f4
pengzhe <pengzhepost@sina.com>
2020-10-11 15:53:40 +0800

    LinkedListCycleIi_142
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/LinkedListCycleIi_142.java
+++ editor/cn/LinkedListCycleIi_142.java
@@ -62,4 +62,25 @@ public class LinkedListCycleIi_142 {
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
+    /**
+     * 快慢指针
+     */
+    class Solution2 {
+        public ListNode detectCycle(ListNode head) {
+            ListNode fast = head, slow = head;
+            while (true) {
+                if (fast == null || fast.next == null) return null;
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow) break;
+            }
+            fast = head;
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return fast;
+        }
+    }
+
 }
