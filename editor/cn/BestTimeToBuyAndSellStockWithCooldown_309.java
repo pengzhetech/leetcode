e3341e05d7163afb5305aa980b85ecb20be16188
pengzhe <pengzhepost@sina.com>
2020-10-28 22:36:07 +0800

    UniquePaths_62
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/BestTimeToBuyAndSellStockWithCooldown_309.java
+++ editor/cn/BestTimeToBuyAndSellStockWithCooldown_309.java
@@ -23,7 +23,25 @@ public class BestTimeToBuyAndSellStockWithCooldown_309 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int maxProfit(int[] prices) {
-            return 0;
+            int len = prices.length;
+            if (len < 2) {
+                return 0;
+            }
+
+            int[][] dp = new int[len][3];
+            dp[0][0] = 0;
+            dp[0][1] = -prices[0];
+            dp[0][2] = 0;
+
+            // dp[i][0]: 手上不持有股票，并且今天不是由于卖出股票而不持股，我们拥有的现金数
+            // dp[i][1]: 手上持有股票时，我们拥有的现金数
+            // dp[i][2]: 手上不持有股票，并且今天是由于卖出股票而不持股，我们拥有的现金数
+            for (int i = 1; i < len; i++) {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2]);
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+                dp[i][2] = dp[i - 1][1] + prices[i];
+            }
+            return Math.max(dp[len - 1][0], dp[len - 1][2]);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
