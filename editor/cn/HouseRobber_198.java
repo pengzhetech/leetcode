a074d3667cef752d9471ccd3b1632a5e75afa4a3
pengzhe <pengzhepost@sina.com>
2020-10-28 22:53:41 +0800

    HouseRobber_198
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/HouseRobber_198.java
+++ editor/cn/HouseRobber_198.java
@@ -39,8 +39,26 @@ public class HouseRobber_198 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int rob(int[] nums) {
-            return 0;
+            if (nums.length == 0) {
+                return 0;
+            }
+            // 子问题：
+            // f(k) = 偷 [0..k) 房间中的最大金额
+
+            // f(0) = 0
+            // f(1) = nums[0]
+            // f(k) = max{ rob(k-1), nums[k-1] + rob(k-2) }
+
+            int N = nums.length;
+            int[] dp = new int[N + 1];
+            dp[0] = 0;
+            dp[1] = nums[0];
+            for (int k = 2; k <= N; k++) {
+                dp[k] = Math.max(dp[k - 1], nums[k - 1] + dp[k - 2]);
+            }
+            return dp[N];
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
