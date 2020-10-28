97e03d5ad68d35f688c24bc44b5831e1a76c65c7
pengzhe <pengzhepost@sina.com>
2020-10-28 23:06:04 +0800

    LongestValidParentheses_32
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/LongestValidParentheses_32.java
+++ editor/cn/LongestValidParentheses_32.java
@@ -24,9 +24,22 @@ public class LongestValidParentheses_32 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int longestValidParentheses(String s) {
-            return 0;
+            int maxans = 0;
+            int[] dp = new int[s.length()];
+            for (int i = 1; i < s.length(); i++) {
+                if (s.charAt(i) == ')') {
+                    if (s.charAt(i - 1) == '(') {
+                        dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                    } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                        dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                    }
+                    maxans = Math.max(maxans, dp[i]);
+                }
+            }
+            return maxans;
         }
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
