81e5eabfc69bbe9d50fa44c80a23e1ace876f40f
pengzhe <pengzhepost@sina.com>
2020-10-28 22:45:27 +0800

    MinimumPathSum_64
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/MinimumPathSum_64.java
+++ editor/cn/MinimumPathSum_64.java
@@ -24,8 +24,17 @@ public class MinimumPathSum_64 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int minPathSum(int[][] grid) {
-            return 0;
+            for(int i = 0; i < grid.length; i++) {
+                for(int j = 0; j < grid[0].length; j++) {
+                    if(i == 0 && j == 0) continue;
+                    else if(i == 0)  grid[i][j] = grid[i][j - 1] + grid[i][j];
+                    else if(j == 0)  grid[i][j] = grid[i - 1][j] + grid[i][j];
+                    else grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                }
+            }
+            return grid[grid.length - 1][grid[0].length - 1];
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
