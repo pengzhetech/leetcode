016c90a120a28b860920cc7aefb306f9b5211da2
pengzhe <pengzhepost@sina.com>
2020-10-28 23:02:54 +0800

    BurstBalloons_312
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/PartitionEqualSubsetSum_416.java
+++ editor/cn/PartitionEqualSubsetSum_416.java
@@ -39,7 +39,34 @@ public class PartitionEqualSubsetSum_416 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean canPartition(int[] nums) {
-            return false;
+            int len = nums.length;
+            if (len == 0) {
+                return false;
+            }
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
+            if ((sum & 1) == 1) {
+                return false;
+            }
+
+            int target = sum / 2;
+            boolean[] dp = new boolean[target + 1];
+            dp[0] = true;
+
+            if (nums[0] <= target) {
+                dp[nums[0]] = true;
+            }
+            for (int i = 1; i < len; i++) {
+                for (int j = target; nums[i] <= j; j--) {
+                    if (dp[target]) {
+                        return true;
+                    }
+                    dp[j] = dp[j] || dp[j - nums[i]];
+                }
+            }
+            return dp[target];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
