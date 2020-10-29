871947e316b7003adf52aa2ac16519f70624046b
pengzhe <pengzhepost@sina.com>
2020-10-29 23:16:48 +0800

    CombinationSum_39
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/CombinationSum_39.java
+++ editor/cn/CombinationSum_39.java
@@ -42,7 +42,7 @@
 // Related Topics 数组 回溯算法 
 // 👍 763 👎 0
 
-import java.util.List;
+import java.util.*;
 
 public class CombinationSum_39 {
     public static void main(String[] args) {
@@ -51,9 +51,38 @@ public class CombinationSum_39 {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
-            return null;
+            int len = candidates.length;
+            List<List<Integer>> res = new ArrayList<>();
+            if (len == 0) {
+                return res;
+            }
+
+            // 排序是剪枝的前提
+            Arrays.sort(candidates);
+            Deque<Integer> path = new ArrayDeque<>();
+            dfs(candidates, 0, len, target, path, res);
+            return res;
         }
+
+        private void dfs(int[] candidates, int begin, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+            // 由于进入更深层的时候，小于 0 的部分被剪枝，因此递归终止条件值只判断等于 0 的情况
+            if (target == 0) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+            for (int i = begin; i < len; i++) {
+                // 重点理解这里剪枝，前提是候选数组已经有序，
+                if (target - candidates[i] < 0) {
+                    break;
+                }
+                path.addLast(candidates[i]);
+                dfs(candidates, i, len, target - candidates[i], path, res);
+                path.removeLast();
+            }
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
