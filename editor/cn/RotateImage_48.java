e55bef3de168657c306d78681fdf0cf9172d221c
pengzhe <pengzhepost@sina.com>
2020-10-30 00:08:04 +0800

    RotateImage_48
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/RotateImage_48.java
+++ editor/cn/RotateImage_48.java
@@ -44,16 +44,42 @@
 // Related Topics 数组 
 // 👍 494 👎 0
 
-public class RotateImage_48{
-      public static void main(String[] args) {
-           Solution solution = new RotateImage_48().new Solution();
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public void rotate(int[][] matrix) {
+public class RotateImage_48 {
+    public static void main(String[] args) {
+        Solution solution = new RotateImage_48().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void rotate(int[][] matrix) {
+            int n = matrix.length;
+            // 上下翻转，两种写法
+            // 第一种
+            //  for (int i =0; i < n /2 ; i++ ){
+            //      for (int j =0; j < n; j ++){
+            //          int tmp = matrix[i][j];
+            //          matrix[i][j] = matrix[n-i-1][j];
+            //          matrix[n-i-1][j] = tmp;
+            //      }
+            //  }
+            // 第二种
+            for (int i = 0; i < n / 2; i++) {
+                int[] tmp = matrix[i];
+                matrix[i] = matrix[n - i - 1];
+                matrix[n - i - 1] = tmp;
+            }
+            //System.out.println(Arrays.deepToString(matrix));
+            // 对角翻转
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = tmp;
+                }
+            }
+        }
 
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}
