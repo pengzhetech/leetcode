86905e4930e6cdc6dcb8aef61100861a9a7b784c
pengzhe <pengzhepost@sina.com>
2020-11-01 22:17:47 +0800

    LongestConsecutiveSequence_128
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/LongestConsecutiveSequence_128.java
+++ editor/cn/LongestConsecutiveSequence_128.java
@@ -10,6 +10,9 @@
 // Related Topics 并查集 数组 
 // 👍 461 👎 0
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestConsecutiveSequence_128 {
     public static void main(String[] args) {
         Solution solution = new LongestConsecutiveSequence_128().new Solution();
@@ -18,7 +21,28 @@ public class LongestConsecutiveSequence_128 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int longestConsecutive(int[] nums) {
-            return 0;
+            Set<Integer> num_set = new HashSet<>();
+            for (int num : nums) {
+                num_set.add(num);
+            }
+
+            int longestStreak = 0;
+
+            for (int num : num_set) {
+                if (!num_set.contains(num - 1)) {
+                    int currentNum = num;
+                    int currentStreak = 1;
+
+                    while (num_set.contains(currentNum + 1)) {
+                        currentNum += 1;
+                        currentStreak += 1;
+                    }
+
+                    longestStreak = Math.max(longestStreak, currentStreak);
+                }
+            }
+
+            return longestStreak;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
