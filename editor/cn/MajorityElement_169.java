c7dbc4fa902781cdecd800840e7f3c364cbd7c74
pengzhe <pengzhepost@sina.com>
2020-11-01 22:30:38 +0800

    LruCache_146
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/MajorityElement_169.java
+++ editor/cn/MajorityElement_169.java
@@ -17,6 +17,9 @@
 // Related Topics 位运算 数组 分治算法 
 // 👍 669 👎 0
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MajorityElement_169 {
     public static void main(String[] args) {
         Solution solution = new MajorityElement_169().new Solution();
@@ -24,10 +27,32 @@ public class MajorityElement_169 {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        private Map<Integer, Integer> countNums(int[] nums) {
+            Map<Integer, Integer> counts = new HashMap<>();
+            for (int num : nums) {
+                if (!counts.containsKey(num)) {
+                    counts.put(num, 1);
+                } else {
+                    counts.put(num, counts.get(num) + 1);
+                }
+            }
+            return counts;
+        }
+
         public int majorityElement(int[] nums) {
-            return 0;
+            Map<Integer, Integer> counts = countNums(nums);
+
+            Map.Entry<Integer, Integer> majorityEntry = null;
+            for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+                if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                    majorityEntry = entry;
+                }
+            }
+
+            return majorityEntry.getKey();
         }
     }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
--- editor/cn/WordBreak_139.java
+++ editor/cn/WordBreak_139.java
@@ -30,7 +30,9 @@
 // Related Topics 动态规划 
 // 👍 591 👎 0
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WordBreak_139 {
     public static void main(String[] args) {
@@ -40,8 +42,20 @@ public class WordBreak_139 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public boolean wordBreak(String s, List<String> wordDict) {
-            return false;
+            Set<String> wordDictSet = new HashSet(wordDict);
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[0] = true;
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[s.length()];
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
