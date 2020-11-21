cccd80478a02466d192ce89f4fd46a4123b01e6d
pengzhe <pengzhepost@sina.com>
2020-11-21 22:42:31 +0800

    PathSumIii_437
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/PathSumIii_437.java
+++ editor/cn/PathSumIii_437.java
@@ -68,14 +68,16 @@ public class PathSumIii_437 {
          * 前缀和的递归回溯思路
          * 从当前节点反推到根节点(反推比较好理解，正向其实也只有一条)，有且仅有一条路径，因为这是一棵树
          * 如果此前有和为currSum-target,而当前的和又为currSum,两者的差就肯定为target了
-         * 所以前缀和对于当前路径来说是唯一的，当前记录的前缀和，在回溯结束，回到本层时去除，保证其不影响其他分支的结果
+         * 所以前缀和对于当前路径来说是唯一的，当前记录的前缀和，
+         * 在回溯结束，回到本层时去除，保证其不影响其他分支的结果
          * @param node 树节点
          * @param prefixSumCount 前缀和Map
          * @param target 目标值
          * @param currSum 当前路径和
          * @return 满足题意的解
          */
-        private int recursionPathSum(TreeNode node, Map<Integer, Integer> prefixSumCount, int target, int currSum) {
+        private int recursionPathSum(TreeNode node, Map<Integer, Integer> prefixSumCount,
+                                     int target, int currSum) {
             // 1.递归终止条件
             if (node == null) {
                 return 0;
@@ -87,11 +89,14 @@ public class PathSumIii_437 {
 
             //---核心代码
             // 看看root到当前节点这条路上是否存在节点前缀和加target为currSum的路径
-            // 当前节点->root节点反推，有且仅有一条路径，如果此前有和为currSum-target,而当前的和又为currSum,两者的差就肯定为target了
-            // currSum-target相当于找路径的起点，起点的sum+target=currSum，当前点到起点的距离就是target
+            // 当前节点->root节点反推，有且仅有一条路径，如果此前有和为currSum-target,
+            // 而当前的和又为currSum,两者的差就肯定为target了
+            // currSum-target相当于找路径的起点，起点的sum+target=currSum，
+            // 当前点到起点的距离就是target
             res += prefixSumCount.getOrDefault(currSum - target, 0);
             // 更新路径上当前节点前缀和的个数
-            prefixSumCount.put(currSum, prefixSumCount.getOrDefault(currSum, 0) + 1);
+            prefixSumCount.put(currSum, prefixSumCount.getOrDefault(currSum,
+                    0) + 1);
             //---核心代码
 
             // 3.进入下一层
