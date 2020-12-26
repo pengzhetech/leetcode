01dd93ba3a89af785a8fb8c6432717ce421fffe8
pengzhe <pengzhepost@sina.com>
2020-12-26 23:55:08 +0800

    LongestValidParentheses_32
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/LongestSubstringWithoutRepeatingCharacters_3.java
+++ editor/cn/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -30,12 +30,10 @@ import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters_3 {
     public static void main(String[] args) {
-        // Solution solution = new LongestSubstringWithoutRepeatingCharacters_3().new Solution();
-        //int i = solution.lengthOfLongestSubstring("12312423423423");
-        //System.out.println(i);
-        //
-        int pwwkew = lengthOfLongestSubstring("pwwkew");
-        System.out.println(pwwkew);
+        Solution solution = new LongestSubstringWithoutRepeatingCharacters_3().new Solution();
+        int i = solution.lengthOfLongestSubstring("12312423423423");
+        System.out.println(i);
+
     }
 
     public static int lengthOfLongestSubstring(String s) {
@@ -55,28 +53,31 @@ public class LongestSubstringWithoutRepeatingCharacters_3 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int lengthOfLongestSubstring(String s) {
-            // 哈希集合，记录每个字符是否出现过
-            Set<Character> occ = new HashSet<Character>();
-            int n = s.length();
-            // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
-            int rk = -1, ans = 0;
-            for (int i = 0; i < n; ++i) {
+            //存储字符,保证没有重复的出现
+            Set<Character> set = new HashSet<>();
+
+            int length = s.length();
+            //右指针,开始位于字符串最左边
+            int rk = -1;
+            int ans = 0;
+
+            //遍历整个字符串
+            for (int i = 0; i < length; ++i) {
+                //如果i不等于0,说明已经遇到一个重复的字符了,此时,滑动窗口
                 if (i != 0) {
-                    // 左指针向右移动一格，移除一个字符
-                    occ.remove(s.charAt(i - 1));
+                    set.remove(s.charAt(i - 1));
                 }
-                while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
-                    // 不断地移动右指针
-                    occ.add(s.charAt(rk + 1));
+                //右指针不断向右移
+                while (rk + 1 < length && !set.contains(s.charAt(rk + 1))) {
                     ++rk;
+                    set.add(s.charAt(rk + 1));
                 }
-                // 第 i 到 rk 个字符是一个极长的无重复字符子串
                 ans = Math.max(ans, rk - i + 1);
             }
+
             return ans;
         }
     }
-
 
 //leetcode submit region end(Prohibit modification and deletion)
 
