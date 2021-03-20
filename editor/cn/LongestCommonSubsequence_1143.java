886201e151084c2a33f7154a97b7763da9e3b1b6
pengzhe <pengzhepost@sina.com>
2021-03-20 22:36:08 +0800

    1143
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/LongestCommonSubsequence_1143.java
+++ editor/cn/LongestCommonSubsequence_1143.java
@@ -49,7 +49,27 @@ public class LongestCommonSubsequence_1143 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int longestCommonSubsequence(String text1, String text2) {
-            return 0;
+
+            int m = text1.length();
+            int n = text2.length();
+
+            int[][] dp = new int[m + 1][n + 1];
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    char c1 = text1.charAt(i);
+                    char c2 = text2.charAt(j);
+
+                    if (c1 == c2) {
+                        dp[i + 1][j + 1] = dp[i][j] + 1;
+
+                    } else {
+                        dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                    }
+                }
+            }
+
+            return dp[m][n];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
