76e84fcc9cc2fe5eb33ae63f089f8959f9f16a8e
pengzhe <pengzhepost@sina.com>
2021-03-21 09:44:45 +0800

    MinimumPathSum_64
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/MinimumPathSum_64.java
+++ editor/cn/MinimumPathSum_64.java
@@ -24,11 +24,11 @@ public class MinimumPathSum_64 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int minPathSum(int[][] grid) {
-            for(int i = 0; i < grid.length; i++) {
-                for(int j = 0; j < grid[0].length; j++) {
-                    if(i == 0 && j == 0) continue;
-                    else if(i == 0)  grid[i][j] = grid[i][j - 1] + grid[i][j];
-                    else if(j == 0)  grid[i][j] = grid[i - 1][j] + grid[i][j];
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (i == 0 && j == 0) continue;
+                    else if (i == 0) grid[i][j] = grid[i][j - 1] + grid[i][j];
+                    else if (j == 0) grid[i][j] = grid[i - 1][j] + grid[i][j];
                     else grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
                 }
             }
@@ -37,5 +37,35 @@ public class MinimumPathSum_64 {
 
     }
 //leetcode submit region end(Prohibit modification and deletion)
+
+
+    public int minPathSum(int[][] grid) {
+
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+
+        int rows = grid.length;
+        int columns = grid[0].length;
+
+        int[][] dp = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+               /* if (i == 0 && j == 0) continue;
+                else if (i == 0) grid[i][j] = grid[i][j - 1] + grid[i][j];
+                else if (j == 0) grid[i][j] = grid[i - 1][j] + grid[i][j];
+                else grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];*/
+                if (i == 0 && j == 0) continue;
+                else if (i == 0) {
+                    grid[i][j] = grid[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    grid[i][j] = grid[i - 1][j] + grid[i][j];
+                } else {
+                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return grid[rows - 1][columns - 1];
+    }
 
 }
