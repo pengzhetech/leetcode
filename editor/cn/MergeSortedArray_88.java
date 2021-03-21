39455ccce69c9a4019acb6490f1f883703555036
pengzhe <pengzhepost@sina.com>
2021-03-21 15:19:26 +0800

    MergeSortedArray_88
    Signed-off-by: pengzhe <pengzhepost@sina.com>

--- editor/cn/MergeSortedArray_88.java
+++ editor/cn/MergeSortedArray_88.java
@@ -21,16 +21,38 @@
 // Related Topics 数组 双指针 
 // 👍 563 👎 0
 
-public class MergeSortedArray_88{
-      public static void main(String[] args) {
-           Solution solution = new MergeSortedArray_88().new Solution();
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+import java.util.Arrays;
 
+public class MergeSortedArray_88 {
+    public static void main(String[] args) {
+        Solution solution = new MergeSortedArray_88().new Solution();
+        int[] array1 = {1, 2, 3, 7, 0, 0, 0};
+        int m = 4;
+        int[] array2 = {4, 5, 6};
+        int n = 3;
+        solution.merge(array1, m, array2, n);
+        System.out.println(Arrays.toString(array1));
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            while (n > 0 && m > 0) {
+                if (nums1[m - 1] > nums2[n - 1]) {
+                    nums1[n + m - 1] = nums1[m - 1];
+                    m--;
+                } else {
+                    nums1[n + m - 1] = nums2[n - 1];
+                    n--;
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
+            }
+            System.out.println(Arrays.toString(nums1));
+
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}
